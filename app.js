8960c2ab76fc1e88088469561032108531a64271
Elena Moron <emoron@hiberus.com>
2021-12-02 08:36:30 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('I am doing an AWS tutorial :D'));
+app.get('/', (req, res) => res.send('AWS Second change ^^'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
